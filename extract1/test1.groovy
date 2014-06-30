84bc1eeea5aeac48581f8e2229070663352f7b49
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-06-30 22:54:14 +0100

    More work on virt conn

--- extract1/test1.groovy
+++ extract1/test1.groovy
@@ -17,10 +17,17 @@ import org.apache.http.entity.mime.*
 import org.apache.http.entity.mime.content.*
 import java.nio.charset.Charset
 import static groovy.json.JsonOutput.*
+import virtuoso.jena.driver.*;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.* ;
+import com.hp.hpl.jena.graph.*;
+
 
 // def the_base_url = "http://sheffieldairquality.gen2training.co.uk/sheffield/index.html"
 // def the_base_url = "http://sheffieldairquality.gen2training.co.uk/sheffield/content.html"
 def the_base_url = "http://sheffieldairquality.gen2training.co.uk"
+
+def store_uri = "jdbc:virtuoso://localhost:1111"
 
 
 println "Loading page"
@@ -41,6 +48,9 @@ println("\n\n All Done");
 
 def processSensorClusterFrameset(base,uri, name) {
   // Fetch page
+
+  validateSensorCluster(uri,name);
+
   try {
     def sensor_cluster_frameset = new XmlParser( new org.cyberneko.html.parsers.SAXParser() ).parse(base+uri)
     sensor_cluster_frameset.FRAMESET.FRAME.each {
@@ -70,12 +80,45 @@ def processSensorCluster(base, uri, name) {
       // For this sensor - work out if we already have a sensor header record in the database
 
       // http://sheffieldairquality.gen2training.co.uk/cgi-bin/gifgraph_sheffield.cgi/data.txt?format=csv&zmacro=Groundhog1/LD-Groundhog1_NO2.ic&from=000101&to=140630
+      def sensorUri = "uri://opensheffield.org/datagrid/sensors/${it.@value}"
       def from="01/01/1900"
       def to="01/06/2014"
       println("DATA URL WILL BE ${base}/cgi-bin/gifgraph_sheffield.cgi/data.txt?format=csv&zmacro=${it.@value}&from=${from}&to=${to}")
+
+      processSensor(sensorUri);
     }
   }
   catch ( Exception e ) {
     println("ERROR...."+e.message);
   }
+}
+
+def processSensor(sensorUri) {
+  // def graph = new VirtGraph(sensorUri, "jdbc:virtuoso://localhost:1111", "dba", "dba");
+  def graph = new VirtGraph('uri://opensheffield.org/datagrid/sensors', "jdbc:virtuoso://localhost:1111", "dba", "dba");
+  // see https://www.google.co.uk/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CCcQFjAA&url=http%3A%2F%2Fwww.w3.org%2F2005%2FIncubator%2Fssn%2Fwiki%2Fimages%2F2%2F2e%2FSemanticSensorNetworkOntology.pdf&ei=AcexU6e1CY3sO8D6gPAM&usg=AFQjCNHrD8E9qlXEk_bU0kwuLNtNbVC5ng&bvm=bv.69837884,d.ZWU
+  // See http://www.w3.org/2005/Incubator/ssn/ssnx/ssn
+  Node sensorUri = Node.createURI(sensorUri);
+  Node class_sensing_device = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#SensingDevice');
+  Node class_observation_value = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#ObservationValue');
+  Node class_observation_value = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#ObservationValue');
+  Node type_pred = Node.createURI('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+
+  graph.add(new Triple(sensorUri, type_pred, class_sensing_device));
+
+  graph.close();
+}
+
+def validateSensorCluster(uri, name) {
+  println("validateSensorCluster ${uri}, ${name}");
+}
+
+def validateSensor(uri, name, type, cluster) {
+  println("validateSensor ${uri}, ${name}, ${type}, ${cluster}");
+  def result = null;
+  result
+}
+
+def validateReading(sensorUri, date, time, value) {
+  println("validateSensor ${sensorUri}, ${date}, ${time}, ${value}");
 }
