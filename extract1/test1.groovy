99b087bcab132339265b6d4413f7c9e36ce0b50f
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-07-01 19:04:06 +0100

    working upload of data

--- extract1/test1.groovy
+++ extract1/test1.groovy
@@ -29,10 +29,16 @@ import virtuoso.jena.driver.*;
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.* ;
 import com.hp.hpl.jena.graph.*;
+import java.text.SimpleDateFormat
 
 // Query:
 // http://localhost:8890/sparql?default-graph-uri=&query=select+distinct+%3Fg+%3Fs+%3Fp+%3Fo+where+%7B+graph+%3Fg+%7B+%3Fs+%3Fp+%3Fo.+%3Fs+a+%3Chttp%3A%2F%2Fpurl.oclc.org%2FNET%2Fssnx%2Fssn%23SensingDevice%3E+%7D+%7D+LIMIT+100&format=text%2Fhtml&timeout=0&debug=on
 // See https://jena.apache.org/tutorials/sparql_datasets.html
+// select distinct ?g ?s ?p ?o where { graph ?g { ?s ?p ?o. ?s a <http://purl.oclc.org/NET/ssnx/ssn#SensingDevice> } } LIMIT 100
+// select distinct ?g ?s ?p ?o where { graph ?g { ?s ?p ?o. ?s a <http://purl.oclc.org/NET/ssnx/ssn#ObservationValue> } } LIMIT 100
+
+// All observation values
+// select distinct ?g ?s ?observationValue where { graph ?g { ?s <http://purl.oclc.org/NET/ssnx/ssn#hasValue> ?observationValue. ?s a <http://purl.oclc.org/NET/ssnx/ssn#ObservationValue> } } LIMIT 100
 
 // def the_base_url = "http://sheffieldairquality.gen2training.co.uk/sheffield/index.html"
 // def the_base_url = "http://sheffieldairquality.gen2training.co.uk/sheffield/content.html"
@@ -145,6 +151,10 @@ def processSensor(sensorUriString, sensorLocalId, sensorDataBaseUrl) {
     // def graph = new VirtGraph(sensorUri, "jdbc:virtuoso://localhost:1111", "dba", "dba");
     // def graph = new VirtGraph('uri://opensheffield.org/datagrid/sensors', "jdbc:virtuoso://localhost:1111", "dba", "dba");
     def graph = new VirtGraph('uri://opensheffield.org/datagrid/sensors', "jdbc:virtuoso://localhost:1111", "dba", "dba");
+
+    def sdf = new SimpleDateFormat('yyMMddhhmm')
+    def reading_uri_format = new SimpleDateFormat('yyyyMMddhhmm')
+    def reading_date_format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
   
     Node class_sensing_device = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#SensingDevice');
     Node class_observation_value = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#ObservationValue');
@@ -153,6 +163,8 @@ def processSensor(sensorUriString, sensorLocalId, sensorDataBaseUrl) {
     Node dc_title_pred = Node.createURI('http://purl.org/metadata/dublin_core#Title');
     Node max_timestamp = Node.createURI('uri://opensheffield.org/properties#maxTimestamp');
     Node last_check = Node.createURI('uri://opensheffield.org/properties#lastCheck');
+    Node has_value_pred = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#hasValue');
+    Node end_time_pred = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#endTime');
   
     type_map.each { key,value ->
       // Create a uri for each of our measurement types
@@ -163,12 +175,6 @@ def processSensor(sensorUriString, sensorLocalId, sensorDataBaseUrl) {
   
     // see https://www.google.co.uk/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CCcQFjAA&url=http%3A%2F%2Fwww.w3.org%2F2005%2FIncubator%2Fssn%2Fwiki%2Fimages%2F2%2F2e%2FSemanticSensorNetworkOntology.pdf&ei=AcexU6e1CY3sO8D6gPAM&usg=AFQjCNHrD8E9qlXEk_bU0kwuLNtNbVC5ng&bvm=bv.69837884,d.ZWU
     // See http://www.w3.org/2005/Incubator/ssn/ssnx/ssn
-    Node sensorUri = Node.createURI(sensorUriString);
-  
-    graph.add(new Triple(sensorUri, type_pred, class_sensing_device));
-    // graph.add(new Triple(sensorUri, measurement_property_pred, class_sensing_device));
-    graph.add(new Triple(sensorUri, max_timestamp, NodeFactory.createLiteral('0')));
-    graph.add(new Triple(sensorUri, last_check, NodeFactory.createLiteral('0')));
   
     // Fetch all entries since last_check
     def from="01/01/1900"
@@ -177,22 +183,69 @@ def processSensor(sensorUriString, sensorLocalId, sensorDataBaseUrl) {
     println("Attempting to fetch ${data_url_str}");
     def data_url = new URL(data_url_str);
     def line = null
-  
-    def process = false
-    data_url.withReader { br ->
-      while ( ( line = br.readLine() ) != null ) {
-        if ( process ) {
-          cells = line.split(",");
-          println("DATA: ${cells}");
-        }
-        else {
-          println("HEADER...");
-        }
 
-        if ( line.startsWith('EOH') )
-          process=true
-
+    def identified_type = null;
+    type_map.each {
+      if ( identified_type == null && 
+           sensorLocalId.toLowerCase().contains(it.key.toLowerCase()) ) {
+        identified_type = it.value;
       }
+    }
+  
+    if ( identified_type ) {
+      Node sensorUri = Node.createURI(sensorUriString);
+      graph.add(new Triple(sensorUri, type_pred, class_sensing_device));
+      graph.add(new Triple(sensorUri, measurement_property_pred, Node.createURI(identified_type.uri)));
+      graph.add(new Triple(sensorUri, max_timestamp, NodeFactory.createLiteral('0')));
+      graph.add(new Triple(sensorUri, last_check, NodeFactory.createLiteral('0')));
+
+      def process = false
+      data_url.withReader { br ->
+        while ( ( line = br.readLine() ) != null ) {
+          if ( line.startsWith( 'EOF') )
+            process=false;
+       
+          if ( process ) {
+            try {
+              cells = line.split(",");
+              println("DATA: ${cells}");
+              def date = sdf.parse(cells[0]+cells[1]);
+              // def hour = cells[1].substring(0,2);
+              // def min = cells[1].substring(2,4);
+  
+              int i=2;
+              while ( i < cells.length ) {
+                println("${date} \"${cells[i]}\" (out of ${cells.length})");
+                if ( cells[i].trim().length() > 0 ) {
+                  println("Publish.. ${reading_uri_format.format(date)} ${cells[i]}");
+                  Node measurement_uri = Node.createURI(sensorUriString+'/'+reading_uri_format.format(date))
+                  graph.add(new Triple(measurement_uri, type_pred, class_observation_value));
+                  graph.add(new Triple(measurement_uri, has_value_pred, NodeFactory.createLiteral(cells[i].trim())));
+                  graph.add(new Triple(measurement_uri, end_time_pred, NodeFactory.createLiteral("${reading_date_format.format(date)}")));
+                  // Reading was made by sensor ${sensorUri}
+                  // Timestamp : date.getTime()
+                  // Measurement : cells[i].trim()
+                }
+                i++
+              }
+  
+            }
+            catch ( Exception e ) {
+              e.printStackTrace()
+            }
+          }
+          else {
+            println("HEADER...");
+          }
+  
+          if ( line.startsWith('EOH') )
+            process=true
+  
+        }
+      }
+    }
+    else {
+      println("Not processed - unable to identify measurement type for ${sensorLocalId}");
     }
   
     graph.close();
