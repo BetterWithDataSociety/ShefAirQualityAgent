9904afc974f4508819408bc0923ff3c73839ab4b
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-07-01 17:21:26 +0100

    Processing all data.....

--- extract1/test1.groovy
+++ extract1/test1.groovy
@@ -93,7 +93,7 @@ def processSensorCluster(base, uri, name) {
       def to="01/06/2014"
       println("DATA URL WILL BE ${base}/cgi-bin/gifgraph_sheffield.cgi/data.txt?format=csv&zmacro=${it.@value}&from=${from}&to=${to}")
 
-      processSensor(sensorUri);
+      processSensor(sensorUri, it.@value, base);
     }
   }
   catch ( Exception e ) {
@@ -101,21 +101,91 @@ def processSensorCluster(base, uri, name) {
   }
 }
 
-def processSensor(sensorUriString) {
-  println("Process Sensor....");
-  // def graph = new VirtGraph(sensorUri, "jdbc:virtuoso://localhost:1111", "dba", "dba");
-  // def graph = new VirtGraph('uri://opensheffield.org/datagrid/sensors', "jdbc:virtuoso://localhost:1111", "dba", "dba");
-  def graph = new VirtGraph('uri://opensheffield.org/datagrid/sensors', "jdbc:virtuoso://localhost:1111", "dba", "dba");
-  // see https://www.google.co.uk/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CCcQFjAA&url=http%3A%2F%2Fwww.w3.org%2F2005%2FIncubator%2Fssn%2Fwiki%2Fimages%2F2%2F2e%2FSemanticSensorNetworkOntology.pdf&ei=AcexU6e1CY3sO8D6gPAM&usg=AFQjCNHrD8E9qlXEk_bU0kwuLNtNbVC5ng&bvm=bv.69837884,d.ZWU
-  // See http://www.w3.org/2005/Incubator/ssn/ssnx/ssn
-  Node sensorUri = Node.createURI(sensorUriString);
-  Node class_sensing_device = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#SensingDevice');
-  Node class_observation_value = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#ObservationValue');
-  Node type_pred = Node.createURI('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+def processSensor(sensorUriString, sensorLocalId, sensorDataBaseUrl) {
+  def type_map = [
+    'PM2.5':[
+             shortcode:'PM25',
+             description:'Particulate matter that is 2.5 micrometers in diameter',
+             uri:'http://en.wikipedia.org/wiki/Particulates'],
+    'O3':[
+             shortcode:'O3',
+             description:'Ozone',
+             uri:'http://en.wikipedia.org/wiki/Ozone'],
+    'NO2':[
+             shortcode:'NO2', 
+             description:'Nitrogen Dioxide',
+             uri:'http://en.wikipedia.org/wiki/NO2'],
+    'Pressure':[
+             shortcode:'Pressure', 
+             description:'Pressure',
+             uri:'http://en.wikipedia.org/wiki/Atmospheric_pressure'],
+    'PM25':[
+             // Ian:: Is this correct - PM25 is actually PM2.5?? thats what I've assumed
+             shortcode:'PM25',
+             description:'Particulate matter that is 2.5 micrometers or less in diameter',
+             uri:'http://en.wikipedia.org/wiki/Particulates'],
+    'PM10':[
+             shortcode:'PM10',
+             description:'Particulate matter that is 10 micrometers or less in diameter',
+             uri:'http://en.wikipedia.org/wiki/Particulates'],
+    'CO':[
+             shortcode:'CO',
+             description:'Carbon Monoxide',
+             uri:'http://en.wikipedia.org/wiki/Carbon_monoxide'],
+  ]
 
-  graph.add(new Triple(sensorUri, type_pred, class_sensing_device));
 
-  graph.close();
+
+  try {
+    println("Process Sensor....");
+
+    // def graph = new VirtGraph(sensorUri, "jdbc:virtuoso://localhost:1111", "dba", "dba");
+    // def graph = new VirtGraph('uri://opensheffield.org/datagrid/sensors', "jdbc:virtuoso://localhost:1111", "dba", "dba");
+    def graph = new VirtGraph('uri://opensheffield.org/datagrid/sensors', "jdbc:virtuoso://localhost:1111", "dba", "dba");
+  
+    Node class_sensing_device = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#SensingDevice');
+    Node class_observation_value = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#ObservationValue');
+    Node type_pred = Node.createURI('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+    Node measurement_property_pred = Node.createURI('http://purl.oclc.org/NET/ssnx/ssn#MeasurementProperty');
+    Node dc_title_pred = Node.createURI('http://purl.org/metadata/dublin_core#Title');
+    Node max_timestamp = Node.createURI('uri://opensheffield.org/properties#maxTimestamp');
+    Node last_check = Node.createURI('uri://opensheffield.org/properties#lastCheck');
+  
+    type_map.each { key,value ->
+      // Create a uri for each of our measurement types
+      measurement_type_uri = Node.createURI(value.uri);
+      graph.add(new Triple(measurement_type_uri, type_pred, measurement_property_pred));
+      graph.add(new Triple(measurement_type_uri, dc_title_pred, NodeFactory.createLiteral(value.description)));
+    }
+  
+    // see https://www.google.co.uk/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CCcQFjAA&url=http%3A%2F%2Fwww.w3.org%2F2005%2FIncubator%2Fssn%2Fwiki%2Fimages%2F2%2F2e%2FSemanticSensorNetworkOntology.pdf&ei=AcexU6e1CY3sO8D6gPAM&usg=AFQjCNHrD8E9qlXEk_bU0kwuLNtNbVC5ng&bvm=bv.69837884,d.ZWU
+    // See http://www.w3.org/2005/Incubator/ssn/ssnx/ssn
+    Node sensorUri = Node.createURI(sensorUriString);
+  
+    graph.add(new Triple(sensorUri, type_pred, class_sensing_device));
+    // graph.add(new Triple(sensorUri, measurement_property_pred, class_sensing_device));
+    graph.add(new Triple(sensorUri, max_timestamp, NodeFactory.createLiteral('0')));
+    graph.add(new Triple(sensorUri, last_check, NodeFactory.createLiteral('0')));
+  
+    // Fetch all entries since last_check
+    def from="01/01/1900"
+    def to="01/12/2014"
+    def data_url_str = "${sensorDataBaseUrl}/cgi-bin/gifgraph_sheffield.cgi/data.txt?format=csv&zmacro=${sensorLocalId}&from=${from}&to=${to}"
+    println("Attempting to fetch ${data_url_str}");
+    def data_url = new URL(data_url_str);
+    def line = null
+  
+    data_url.withReader { br ->
+      while ( ( line = br.readLine() ) != null ) {
+        println(line);
+      }
+    }
+  
+    graph.close();
+  }
+  catch ( Exception e ) {
+    e.printStackTrace();
+  }
 }
 
 def validateSensorCluster(uri, name) {
