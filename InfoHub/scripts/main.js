7cf6c12a86ca762650f5647fcc0986f21088928d
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-02-16 13:42:28 +0000

    working framework

---
+++ InfoHub/scripts/main.js
@@ -0,0 +1,12 @@
+angular.module('BWDInfoApp', ['ui.bootstrap']).controller('BWDInfoAppController', function ($scope) {
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+});
