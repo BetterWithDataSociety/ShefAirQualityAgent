1d5029decbdc09f868bcbbd2a5fcb9acd00d007d
boisvert <charles@boisvert.me.uk>
2015-10-18 19:25:34 +0100

    Added unshaql/ - for Sheffield air quality. And it sounds nice.
    unshaql is a web client to see SPARQL syntax highlighted and some help viewing results and choosing return format.

---
+++ unshaql/getbin.php
@@ -0,0 +1,23 @@
+<?php
+
+if (isset($_REQUEST['id'])) {
+   $id = $_REQUEST['id'];
+   $url = "http://pastebin.com/raw.php?i=$id";
+   $ch = curl_init($url);
+
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);  
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_VERBOSE, 1);
+   curl_setopt($ch, CURLOPT_NOBODY, 0);
+ 
+   $response = curl_exec($ch);
+   //echo "here it goes\n";
+   echo $response;
+}
+else
+{
+   echo "no pastebin file id given";
+}
+exit();
+?>
